ec22ecb27577050f078da8f2735cfa3d98c4e6c0
AshutoshPatil <ashutosh.patil786@gmail.com>
2018-01-22 01:58:52 +0530

    hello.java
    Simple start off

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Hello{
+public static void main(String args[]){
+	out.println("HelloWorld");
+	out.println("Enter your Name");
+	Scanner names= new Scanner(System.in);
+	System.out.println(names);
+
+}
+
+}
